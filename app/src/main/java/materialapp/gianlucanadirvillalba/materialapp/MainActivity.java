e24da36a5c2ee3963b36d84714b5f020088c6579
gianluca nadir villalba <gianluca.villalba@hotmail.com>
2016-09-07 15:22:27 +0200

    Added style, appbar and menu

--- app/src/main/java/materialapp/gianlucanadirvillalba/materialapp/MainActivity.java
+++ app/src/main/java/materialapp/gianlucanadirvillalba/materialapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-//PROVA BRANCHES
+
 public class MainActivity extends AppCompatActivity
 {
     private Toolbar toolbar;
